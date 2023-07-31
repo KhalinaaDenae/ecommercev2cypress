bae3c3d269b0812e9064f4aa42e40f476b31002b
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-31 14:53:46 -0400

    initial files

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,27 @@
+// product.spec.js
+describe('Product Component', () => {
+  beforeEach(() => {
+    // Assuming the application is running at http://localhost:8888
+    // This will run before each test in this describe block
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Displays the product image correctly', () => {
+    cy.get('.product-image').should('be.visible');
+  });
+
+  it('Product name is clickable and leads to the correct product page', () => {
+    cy.get('.product-name').should('be.visible').click();
+
+    // Assuming the product page URL follows the pattern '/product/:id'
+    cy.url().should('match', /\/product\/[a-zA-Z0-9]+$/);
+  });
+
+  it('Displays the product price correctly', () => {
+    cy.get('.product-price').should('be.visible').contains('$');
+  });
+
+  it('Contains the expected class name', () => {
+    cy.get('.product-card').should('have.class', 'product-card');
+  });
+});
